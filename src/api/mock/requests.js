3957d2a7eba9b1ec4f1ae844c97dd983cd1f7181
deeem <karpov.dimon@gmail.com>
2019-10-13 11:36:14 +0300

    update requests mock

--- src/api/mock/requests.js
+++ src/api/mock/requests.js
@@ -1,10 +1,12 @@
+import moment from 'moment'
+
 export default [
     {
         id: 1,
-        startsAt: '',
-        stopsAt: '',
+        startsAt: moment().hour(19).valueOf(),
+        stopsAt: moment().hour(20).valueOf(),
         platform: {
-            id: 1,
+            id: 2,
             title: 'SEGA'
         },
         game: {
@@ -19,5 +21,47 @@ export default [
             type: 1 // have not played before
         },
         microphone: true,
-    }
+    },
+    {
+        id: 2,
+        startsAt: moment().hour(17).valueOf(),
+        stopsAt: moment().hour(20).valueOf(),
+        platform: {
+            id: 1,
+            title: 'NES'
+        },
+        game: {
+            id: 1,
+            title: 'Battletoads'
+        },
+        user: {
+            id: 101,
+            name: 'Jane Doe'
+        },
+        skill: {
+            type: 2
+        },
+        microphone: true,
+    },
+    {
+        id: 3,
+        startsAt: moment().hour(18).valueOf(),
+        stopsAt: moment().hour(21).valueOf(),
+        platform: {
+            id: 1,
+            title: 'NES'
+        },
+        game: {
+            id: 1,
+            title: 'Street Fighter 2010'
+        },
+        user: {
+            id: 100,
+            name: 'John Doe'
+        },
+        skill: {
+            type: 1 // have not played before
+        },
+        microphone: true,
+    },
 ]
