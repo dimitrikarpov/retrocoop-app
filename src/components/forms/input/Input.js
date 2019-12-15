25a6c654f3fb1bfcd98185c1830b9fcf3a6e37e7
deeem <karpov.dimon@gmail.com>
2019-12-15 11:18:38 +0200

    add input and select components

---
+++ src/components/forms/input/Input.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Input = props => (
+  <div className='form-group'>
+    <label className='form-label'>{props.title}</label>
+    <input
+      className='form-input'
+      name={props.name}
+      type={props.inputType}
+      value={props.content}
+      onChange={props.controlFunc}
+      placeholder={props.placeholder}
+    />
+  </div>
+)
+
+Input.propTypes = {
+  inputType: React.PropTypes.oneOf(['text', 'number']).isRequired,
+  title: React.PropTypes.string.isRequired,
+  name: React.PropTypes.string.isRequired,
+  controlFunc: React.PropTypes.func.isRequired,
+  content: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.number
+  ]).isRequired,
+  placeholder: React.PropTypes.string
+}
+
+export default Input
+
+// more at https://lorenstewart.me/2016/10/31/react-js-forms-controlled-components/
