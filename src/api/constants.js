8fc8bae0cec106064fd285c63323e4f87e8b990b
deeem <karpov.dimon@gmail.com>
2020-01-21 06:34:51 +0200

    use backend for getting requests

---
+++ src/api/constants.js
@@ -0,0 +1,2 @@
+export const API_REQUESTS_BASE_URL = 'http://localhost:3003/api/'
+export const API_REQUESTS_FETCH_ALL_ENDPOINT = 'requests'
--- src/api/requestsApi.js
+++ src/api/requestsApi.js
@@ -1,21 +1,26 @@
 import axios from 'axios'
 import requestsMock from 'api/mock/requests'
 
+import {
+  API_REQUESTS_BASE_URL as BASE,
+  API_REQUESTS_FETCH_ALL_ENDPOINT as FETCH_ALL
+} from './constants'
+
 const api = axios.create({
-  baseURL: 'https://'
+  baseURL: BASE
 })
 
-// api.getPosts = async () => {
-//   const res = await api.get('/posts')
-
-//   return res.data
+// api.getRequests = () => {
+//   return new Promise((resolve, reject) => {
+//     resolve(requestsMock)
+//     // reject(err)
+//   })
 // }
 
-api.getRequests = () => {
-  return new Promise((resolve, reject) => {
-    resolve(requestsMock)
-    // reject(err)
-  })
+api.getRequests = async () => {
+  const res = await api.get(FETCH_ALL)
+
+  return res.data
 }
 
 export default api
