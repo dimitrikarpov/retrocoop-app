74dafc31576e7ebfd15cfe08d4aafb8c1f91b42a
deeem <karpov.dimon@gmail.com>
2020-01-23 06:26:41 +0200

    use requests service

--- src/pages/requests/Requests.js
+++ src/pages/requests/Requests.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+import RequestService from 'api/services/requests'
 
 class Requests extends Component {
+  componentDidMount() {
+    RequestService.getAll();
+  }
+
   render() {
     return <h1>Requests component</h1>
   }
