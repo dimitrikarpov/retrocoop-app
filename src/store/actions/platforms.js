8b416fe6136f20cca0f8965b6d3005310a39a4f4
deeem <karpov.dimon@gmail.com>
2020-01-24 06:43:58 +0200

    using new platform service

--- src/store/actions/platforms.js
+++ src/store/actions/platforms.js
@@ -1,14 +1,18 @@
 import * as actionTypes from 'store/actions/actionsTypes'
-import api from 'api/platformsApi'
+// import api from 'api/platformsApi'
+import PlatformsService from 'api/services/platforms'
 
 export const fetchPlatforms = () => {
+
+  console.log(PlatformsService)
+
   return async dispatch => {
     dispatch({
       type: actionTypes.FETCH_PLATFORMS_START
     })
 
     try {
-      const platforms = await api.fetchAll()
+      const platforms = await PlatformsService.getAll()
       dispatch({
         type: actionTypes.FETCH_PLATFORMS_SUCCESS,
         payload: platforms.data
