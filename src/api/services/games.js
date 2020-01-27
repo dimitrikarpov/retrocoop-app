71ebbb1a94660df599d93e904af88aa74d625164
deeem <karpov.dimon@gmail.com>
2020-01-27 20:53:26 +0200

    games api service

---
+++ src/api/services/games.js
@@ -0,0 +1,31 @@
+import request from '../request'
+
+const endpoint = '/games'
+
+const getAll = () =>
+  request({
+    url: `${endpoint}`,
+    method: 'GET'
+  })
+
+const get = id =>
+  request({
+    url: `${endpoint}/${id}`,
+    method: 'GET'
+  })
+
+const create = ({ subject, content }) =>
+  request({
+    url: `${endpoint}`,
+    method: 'POST',
+    data: {
+      subject,
+      content
+    }
+  })
+
+export default {
+  getAll,
+  get,
+  create //, update, delete, etc. ...
+}
