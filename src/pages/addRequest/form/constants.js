3fa8a7ff6719e4016afb19fecc5a48dccaba04f9
deeem <karpov.dimon@gmail.com>
2020-02-07 06:10:18 +0200

    form constants.js

---
+++ src/pages/addRequest/form/constants.js
@@ -0,0 +1,22 @@
+import * as yup from 'yup'
+
+export const initialValues = {
+  game: 'ррррлр',
+  platform: '',
+  skill: '',
+  use_mic: true,
+  starts_at: '2020-01-09T05:04',
+  ends_at: '2020-01-09T05:04'
+  // starts_at: '',
+  // ends_at: ''
+}
+
+export const validationSchema = yup.object({
+  game: yup
+    .string()
+    .required()
+    .max(25),
+  platform: yup.number(),
+  starts_at: yup.date().required(),
+  ends_at: yup.date().required()
+})
--- src/pages/addRequest/form/dataForm.js
+++ src/pages/addRequest/form/dataForm.js
@@ -2,42 +2,22 @@ import React from 'react'
 import { shape, arrayOf, string } from 'prop-types'
 import { connect } from 'react-redux'
 import { Formik, Form, Field } from 'formik'
-import * as yup from 'yup'
 import './styles.scss'
 
+import { initialValues, validationSchema } from './constants'
 import GameField from './components/GameField'
 import StartsAtField from './components/StartsAtField'
 import EndsAtField from './components/EndsAtField'
 import PlatformField from './components/PlatformField'
-
 import Checkbox from 'components/forms/checkbox/Checkbox'
 import Radio from 'components/forms/radio/Radio'
 import RequestsService from 'api/services/requests'
 import GamesService from 'api/newservices/games'
 import Autocomplete from 'components/forms/autocomplete/Autocomplete'
 
-const validationSchema = yup.object({
-  game: yup
-    .string()
-    .required()
-    .max(25),
-  platform: yup.number(),
-  starts_at: yup.date().required(),
-  ends_at: yup.date().required()
-})
-
 const DataForm = ({ platforms }) => (
   <Formik
-    initialValues={{
-      game: 'ррррлр',
-      platform: '',
-      skill: '',
-      use_mic: true,
-      starts_at: '2020-01-09T05:04',
-      ends_at: '2020-01-09T05:04'
-      // starts_at: '',
-      // ends_at: ''
-    }}
+    initialValues={initialValues}
     onSubmit={(data, { setSubmitting, resetForm, setErrors }) => {
       setSubmitting(true)
       // make async call
@@ -72,7 +52,10 @@ const DataForm = ({ platforms }) => (
           field='title'
           transformResponse={response => {
             const data = JSON.parse(response)
-            return data.data.map(game => ({ value: game.id, title: game.title }))
+            return data.data.map(game => ({
+              value: game.id,
+              title: game.title
+            }))
           }}
         />
 
@@ -163,5 +146,3 @@ DataForm.propTypes = {
     })
   )
 }
-
-// {"success":true,"count":4,"data":[{"images":["http://127.0.0.1:3003/images/zx-10th-frame/1.gif","http://127.0.0.1:3003/images/zx-10th-frame/2.gif","http://127.0.0.1:3003/images/zx-10th-frame/3.gif","http://127.0.0.1:3003/images/zx-10th-frame/4.gif"],"_id":"5de3f6f4c9af1c2c631dd76c","platform":{"_id":"5de3f6eb293acc2c2fdb7e9e","title":"ZX-Spectrum","slug":"zx","id":"5de3f6eb293acc2c2fdb7e9e"},"title":"10th Frame","description":"Hic modi doloremque temporibus iure aut est consectetur repudiandae. Tenetur ut molestiae sequi sit nam porro accusamus. Ut commodi cumque autem asperiores rerum et quia doloremque. Est laborum voluptates consequatur. Tempore iste laborum molestias ullam.","__v":0,"id":"5de3f6f4c9af1c2c631dd76c"},{"images":["http://127.0.0.1:3003/images/zx-1942/1.gif","http://127.0.0.1:3003/images/zx-1942/2.gif","http://127.0.0.1:3003/images/zx-1942/3.gif"],"_id":"5de3f6f4c9af1c2c631dd76f","platform":{"_id":"5de3f6eb293acc2c2fdb7e9e","title":"ZX-Spectrum","slug":"zx","id":"5de3f6eb293acc2c2fdb7e9e"},"title":"1942","description":"Cupiditate cupiditate aut consequatur et. Vitae ea minus assumenda tempora accusantium ipsam voluptatem est dolor. Ea nobis maiores id ipsa. Aut qui tempore corporis. Et ratione alias ab perferendis adipisci debitis consequuntur.","__v":0,"id":"5de3f6f4c9af1c2c631dd76f"},{"images":["http://127.0.0.1:3003/images/zx-1942-mission/1.gif","http://127.0.0.1:3003/images/zx-1942-mission/2.gif"],"_id":"5de3f6f4c9af1c2c631dd770","platform":{"_id":"5de3f6eb293acc2c2fdb7e9e","title":"ZX-Spectrum","slug":"zx","id":"5de3f6eb293acc2c2fdb7e9e"},"title":"1942 Mission","description":"Quibusdam libero aperiam maiores ut adipisci veritatis possimus et. Rem et nesciunt. Aut quis dolore consequuntur qui nihil et labore nihil quis. Odit rerum velit sed provident et.","__v":0,"id":"5de3f6f4c9af1c2c631dd770"},{"images":["http://127.0.0.1:3003/images/zx-180-mad-games/1.gif","http://127.0.0.1:3003/images/zx-180-mad-games/2.gif","http://127.0.0.1:3003/images/zx-180-mad-games/3.gif"],"_id":"5de3f6f4c9af1c2c631dd76d","platform":{"_id":"5de3f6eb293acc2c2fdb7e9e","title":"ZX-Spectrum","slug":"zx","id":"5de3f6eb293acc2c2fdb7e9e"},"title":"180! (Mad Games)","description":"Sunt reprehenderit quam inventore sapiente totam eos qui voluptatem consectetur. Pariatur ea et voluptatum explicabo voluptatem quaerat neque vero eos. Doloribus deserunt nostrum alias et dolores repellendus inventore voluptas. Magni facilis pariatur est eaque quasi quidem repudiandae reprehenderit tempore. Beatae et et corrupti repudiandae vel rerum delectus veniam. Ex magnam doloremque optio.","__v":0,"id":"5de3f6f4c9af1c2c631dd76d"}],"pagination":{"next":{"page":2,"limit":4}}}
